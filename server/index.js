964ac72bf3eb3bfa1e9b060a22d1f72ec193579a
Enar <enmust@ttu.ee>
2020-01-16 01:07:46 +0200

    server update

--- server/index.js
+++ server/index.js
@@ -24,6 +24,8 @@ const apiProxy = proxy({
 app.use('/api', apiProxy);
 
 // Handle production
+console.log(process.env.NODE_ENV);
+console.log(process.env.PORT);
 if (process.env.NODE_ENV === 'production') {
   // Static folder
   app.use(express.static(__dirname + '/public/'));
@@ -32,6 +34,6 @@ if (process.env.NODE_ENV === 'production') {
   app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'))
 }
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.NODE_ENV === 'production' ? 80 : 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
