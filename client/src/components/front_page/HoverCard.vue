6df09e7048bdcdec4206059bca336f2ba5490cab
Enar <enmust@ttu.ee>
2020-09-21 17:12:46 +0300

    #63 front page updates

---
+++ client/src/components/front_page/HoverCard.vue
@@ -0,0 +1,79 @@
+<template>
+  <v-hover v-slot:default="{ hover }" :close-delay="800">
+    <v-card
+      :elevation="hover ? 12 : 6"
+      tile
+      height="300"
+      hover
+      class="d-flex flex-column"
+      :style="`background-image: url(&quot;${image}&quot;); background-size:cover`"
+    >
+      <v-spacer />
+
+      <v-card-title
+        class="justify-center text-uppercase font-weight-bold animate__animated animate__faster white--text"
+        :class="{
+          animate__slideInUp: hover,
+          animate__slideInDown: !hover
+        }"
+        >{{ title }}</v-card-title
+      >
+
+      <v-card-text
+        v-show="hover"
+        :class="{
+          'animate__slideInUp opacity-1': hover,
+          'animate__slideOutDown opacity-0': !hover
+        }"
+        class="text-center animate__animated animate__faster white--text font-weight-bold"
+        >{{ text }}</v-card-text
+      >
+
+      <v-card-actions
+        v-show="hover"
+        :class="{
+          'animate__slideInUp opacity-1': hover,
+          'animate__slideOutDown opacity-0': !hover
+        }"
+        class="justify-center mb-2 animate__animated animate__faster"
+      >
+        <v-btn dark class="font-weight-bold " elevation="6" :to="url" tile>{{
+          button
+        }}</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-hover>
+</template>
+
+<script>
+export default {
+  name: "HoverCard",
+
+  props: ["title", "text", "button", "url", "image"]
+};
+</script>
+
+<style scoped>
+.opacity-0 {
+  opacity: 0;
+  transition: all 800ms ease;
+}
+
+.opacity-1 {
+  opacity: 1;
+  transition: opacity 800ms ease;
+}
+
+.v-card__title,
+.v-card__text {
+  text-shadow: 2px 2px 4px #000;
+}
+
+.v-card__title {
+  font-size: 1.5rem;
+}
+
+.v-card__text {
+  font-size: 1rem;
+}
+</style>
---
+++ client/src/views/FrontPage.vue
@@ -0,0 +1,78 @@
+<template>
+  <v-container class="FrontPage">
+    <!-- Todo: Info -->
+    <v-row no-gutters>
+      <v-col class="pa-3" cols="12" sm="6" md="3"> </v-col>
+    </v-row>
+
+    <!-- Todo: News etc. -->
+    <!--    <v-card class="mt-2">-->
+    <v-row no-gutters>
+      <v-col
+        class="pa-3"
+        cols="12"
+        sm="6"
+        md="3"
+        v-for="card in cards"
+        :key="card.title"
+      >
+        <HoverCard
+          :title="card.title"
+          :text="card.text"
+          :button="card.button"
+          :url="card.url"
+          :image="card.image"
+        />
+      </v-col>
+    </v-row>
+    <!--    </v-card>-->
+  </v-container>
+</template>
+
+<script>
+import HoverCard from "@/components/front_page/HoverCard";
+export default {
+  name: "FrontPage",
+  components: { HoverCard },
+  data: () => ({
+    cards: [
+      {
+        title: "Fossils",
+        text:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        button: "View Fossils",
+        url:
+          '/search?recordbasis="FossileSpecimen" OR "FossilSpecimen" OR "RecentPreservedSpecimen" OR "fossil"',
+        image: require("@/assets/front_page/fossil.png")
+      },
+      {
+        title: "Minerals",
+        text:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        button: "View Minerals",
+        url: '/search?recordbasis="MineralSpecimen"',
+        image: require("@/assets/front_page/mineral_2.png")
+      },
+      {
+        title: "Rocks",
+        text:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        button: "View Rocks",
+        url:
+          '/search?recordbasis="RockSpecimen" OR "SedimentSample" OR "TechnologicalSample"',
+        image: require("@/assets/front_page/mineral_0.png")
+      },
+      {
+        title: "Meteorites",
+        text:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        button: "View Meteorites",
+        url: '/search?recordbasis="RockSpecimen"',
+        image: require("@/assets/front_page/mineral_1.png")
+      }
+    ]
+  })
+};
+</script>
+
+<style scoped></style>
