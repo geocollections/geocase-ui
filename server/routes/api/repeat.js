91403d63fcd0238a8ef63aa8b48977f180e1cfbb
Enar <enmust@ttu.ee>
2020-10-01 13:17:11 +0300

    #41 xml to json parser

--- server/routes/api/repeat.js
+++ server/routes/api/repeat.js
@@ -3,6 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 const axios = require('axios')
+const xml2js = require('xml2js');
 
 router.get('/', async (req, res) => {
   if (req.query) {
@@ -11,8 +12,13 @@ router.get('/', async (req, res) => {
         let response = await axios.get(req.query.url)
         if (response) {
           if (response.status === 200) {
-            // Todo: XML to JSON
-            if (response.data) res.send({ data: response.data })
+            if (response.data) {
+              let json = response.data;
+              if (response.data.startsWith("<abcd:DataSets xmlns:xsi=")) {
+                json = await xml2js.parseStringPromise(response.data, { mergeAttrs: true });
+              }
+              res.send({ data: json })
+            }
             else res.send({
               error: "No data from response!",
               status: response.status,
@@ -24,15 +30,12 @@ router.get('/', async (req, res) => {
             statusText: response.statusText
           })
         } else res.send({ error: "Request failed!" })
-
-        console.log(response)
         res.send({response: response.status, url: req.query.url})
       } catch (err) {
         res.status(500).send({ error: err.code })
       }
     } else res.status(500).send({ error: "Missing query parameter: url" })
   } else res.status(500).send({ error: "Check your URL!" })
-
 });
 
 module.exports = router;
