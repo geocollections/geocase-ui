4b43feffb12c42699ab5dddf1785e697d6d3b53e
Enar <enmust@ttu.ee>
2020-10-20 14:56:27 +0300

    removed production console logs + added robots.txt

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,9 @@
+const plugins = ["@babel/plugin-proposal-optional-chaining"];
+
+if (process.env.NODE_ENV === "production")
+  plugins.push("transform-remove-console");
+
 module.exports = {
   presets: ["@vue/cli-plugin-babel/preset"],
-  plugins: ["@babel/plugin-proposal-optional-chaining"]
+  plugins: plugins
 };
