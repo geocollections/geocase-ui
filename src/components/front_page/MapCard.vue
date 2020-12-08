6abe3468bceff6312ddaaaf4cf644f1b27945fea
Alexander Buhl <alexander.buhl@mfn.berlin>
2020-12-08 18:13:07 +0100

    add baseMap with attribution & scale control #97

---
+++ src/components/front_page/MapCard.vue
@@ -0,0 +1,65 @@
+<template>
+  <v-card height="100%">
+    <MglMap
+      container="map"
+      class="mapboxgl-map"
+      :mapStyle.sync="mapStyle"
+      :attributionControl="false"
+      @load="onMapLoaded"
+    >
+      <MglAttributionControl position="bottom-right" />
+      <MglScaleControl position="bottom-left" />
+    </MglMap>
+  </v-card>
+</template>
+<!-- :accessToken="accessToken" :mapboxGl="mapbox-gl" -->
+
+<script>
+import Mapbox from "mapbox-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
+import { MglMap, MglAttributionControl, MglScaleControl } from "vue-mapbox";
+
+export default {
+  name: "MapCard",
+  components: {
+    MglMap,
+    MglAttributionControl,
+    MglScaleControl,
+  },
+  data() {
+    return {
+      //accessToken: ACCESS_TOKEN, // your access token. Needed if you using Mapbox maps
+      mapStyle: "https://basemaps.cartocdn.com/gl/positron-gl-style/style.json", // your map style
+      center: [13.399976387004955, 52.51664415491936],
+      zoom: 5,
+      minZoom: 1,
+      maxZoom: 14,
+    };
+  },
+  created() {
+    // We need to set mapbox-gl library here in order to use it in template
+    this.mapbox = Mapbox;
+    //console.log(Mapbox);
+  },
+  methods: {
+    onMapLoaded(event) {
+      // in component
+      this.map = event.map;
+      // or just to store if you want have access from other components
+      this.$store.map = event.map;
+    },
+  },
+};
+</script>
+
+<style>
+.mgl-map-wrapper .mapboxgl-map {
+  position: relative;
+  min-height: 600px;
+  max-height: 800px;
+}
+.mapboxgl-ctrl-scale {
+  border: 2px solid #777;
+  border-top: none;
+}
+</style>
