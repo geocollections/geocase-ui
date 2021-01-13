61472f79945874b65768c5358f357f113f98438b
enar <ents100@gmail.com>
2021-01-13 13:28:06 +0200

    #48 static pages updated

--- src/views/Links.vue
+++ src/views/Links.vue
@@ -12,4 +12,16 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.Links >>> .row {
+  margin: 8px 0;
+}
+
+.Links >>> .col-6:first-child {
+  padding-right: 8px;
+}
+
+.Links >>> .col-6:last-child {
+  padding-left: 8px;
+}
+</style>
