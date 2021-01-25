69e91849a85bf84edf0b060c2cb615deac1c7cbd
enar <ents100@gmail.com>
2021-01-25 18:26:05 +0200

    #147 test prerender plugin

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,6 @@
 const routes = require("./src/router/sitemapRoutes");
+const path = require("path");
+const PrerenderSPAPlugin = require("prerender-spa-plugin");
 
 module.exports = {
   transpileDependencies: ["vuetify"],
@@ -27,5 +29,15 @@ module.exports = {
       },
       routes
     }
+  },
+  configureWebpack: {
+    plugins: [
+      new PrerenderSPAPlugin({
+        // Required - The path to the webpack-outputted app to prerender.
+        staticDir: path.join(__dirname, "dist"),
+        // Required - Routes to render.
+        routes: ["/", "/about", "/howto"]
+      })
+    ]
   }
 };
