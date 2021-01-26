6710a12beb15f95ad07bf4351b9d8ad917ea3ce1
enar <ents100@gmail.com>
2021-01-26 11:20:09 +0200

    #148 added 'beta' to app title

--- src/components/app_markup/header/AppBar.vue
+++ src/components/app_markup/header/AppBar.vue
@@ -49,6 +49,7 @@
                   :class="{ 'small-font': $vuetify.breakpoint.smOnly }"
                   >GeoCASe 2.0
                   <span v-if="!isProductionUrl">DEV</span>
+                  <span v-else>BETA</span>
                 </span>
                 <span class="hidden-sm-and-up">
                   <v-icon>fas fa-home</v-icon>
