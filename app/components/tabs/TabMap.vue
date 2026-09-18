3897d7eec369f2f7c7d6c79595b4a2ad1b538e31
almilo <almilo@taltech.ee>
2026-09-18 14:22:17 +0300

    refactor: migrate search map tab to Nuxt UI

--- app/components/tabs/TabMap.vue
+++ app/components/tabs/TabMap.vue
@@ -1,90 +1,95 @@
+<script setup>
+import { computed } from "vue";
+import { useI18n } from "vue-i18n";
+import { useSearchStore } from "@/stores/search";
+import MapWrapper from "@/components/MapWrapper.vue";
+
+const props = defineProps({
+  responseResults: {
+    type: Array,
+    required: true,
+  },
+  responseResultsCount: {
+    type: Number,
+    default: 1,
+  },
+  isDetailView: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const { t } = useI18n();
+const searchStore = useSearchStore();
+
+const localities = computed(() =>
+  props.responseResultsCount > 0
+    ? props.responseResults.filter((locality) => locality.has_map)
+    : [],
+);
+</script>
+
 <template>
-  <v-card flat>
-    <v-row
-      no-gutters
-      class="my-4"
-      justify="center"
-      v-if="localities.length === 0"
+  <section
+    class="tw:relative tw:min-h-32"
+    :aria-busy="searchStore.isLoading"
+  >
+    <UProgress
+      v-if="searchStore.isLoading"
+      color="primary"
+      size="xs"
+      animation="carousel"
+      class="tw:absolute tw:inset-x-0 tw:top-0 tw:z-10"
+    />
+
+    <div
+      v-if="!searchStore.isLoading && localities.length === 0"
+      class="tw:mx-auto tw:max-w-2xl tw:px-4 tw:py-10"
     >
-      <v-col cols="12" style="max-width: 500px">
-        <v-alert
-          class="mb-0"
-          variant="tonal"
-          border="start"
-          icon="fa:fas fa-search"
-          color="secondary"
-        >
-          <div>
-            {{ $t("search.mapNoResults") }}
-          </div>
-
-          <div v-if="!search.has_map.value">
-            {{ $t("search.mapNoResultsFilterInfo") }}
-            <v-btn
-              size="x-small"
-              color="secondary"
-              @click="updateSearchField({ id: 'has_map', value: 'true' })"
-              >{{ $t("search.addFilter") }}</v-btn
+      <UAlert
+        color="neutral"
+        variant="soft"
+        icon="i-lucide-map-pin-off"
+        :title="t('search.mapNoResults')"
+      >
+        <template #description>
+          <div
+            v-if="!searchStore.search.has_map.value"
+            class="tw:flex tw:flex-col tw:items-start tw:gap-3 tw:sm:flex-row tw:sm:items-center"
+          >
+            <span>{{ t("search.mapNoResultsFilterInfo") }}</span>
+            <UButton
+              size="sm"
+              color="primary"
+              variant="soft"
+              icon="i-lucide-list-filter-plus"
+              @click="
+                searchStore.updateSearchField({
+                  id: 'has_map',
+                  value: 'true',
+                })
+              "
             >
+              {{ t("search.addFilter") }}
+            </UButton>
           </div>
-        </v-alert>
-      </v-col>
-    </v-row>
+        </template>
+      </UAlert>
+    </div>
 
-    <div class="map" v-show="localities.length > 0">
-      <map-wrapper
+    <div
+      v-show="localities.length > 0"
+      role="region"
+      :aria-label="t('search.tab.map')"
+      class="map tw:ring-default tw:overflow-hidden tw:rounded-xl tw:ring-1 tw:shadow-sm"
+    >
+      <MapWrapper
+        class="tw:w-full"
         :response-results="responseResults"
         :response-results-count="responseResultsCount"
         :height="isDetailView ? '50vh' : '70vh'"
         :is-detail-view="isDetailView"
       />
     </div>
-  </v-card>
+  </section>
 </template>
-
-<script>
-import { useSearchStore } from "@/stores/search";
-
-import { mapActions, mapState } from "pinia";
-import MapWrapper from "@/components/MapWrapper.vue";
-
-export default {
-  name: "TabMap",
-  components: { MapWrapper },
-  props: {
-    responseResults: {
-      type: Array,
-      required: true,
-    },
-    responseResultsCount: {
-      type: Number,
-      required: false,
-      default: 1,
-    },
-    isDetailView: {
-      type: Boolean,
-      default: false,
-    },
-  },
-
-  computed: {
-    ...mapState(useSearchStore, ["search"]),
-
-    localities() {
-      if (this.responseResultsCount > 0) {
-        return this.responseResults.filter((locality) => !!locality.has_map);
-      } else return [];
-    },
-  },
-
-  methods: {
-    ...mapActions(useSearchStore, ["updateSearchField"]),
-  },
-};
-</script>
-
-<style scoped>
-.map-progress-circular {
-  transition: margin-left 200ms ease-in-out;
-}
-</style>
